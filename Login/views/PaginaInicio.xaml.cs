2295fdae4d394e04f493985dcaf4e6d6908e0dd9
Emerson1911 <1284662@clases.edu.sv>
2023-09-29 23:06:46 +0200

    Cambio de Carrusel

--- Login/views/PaginaInicio.xaml.cs
+++ Login/views/PaginaInicio.xaml.cs
@@ -10,12 +10,12 @@ public partial class PaginaInicio : ContentPage
 
         var items = new List<CollectionItem>
         {
-            new CollectionItem{Imagen ="ace_rycen5.jpg", Title="Coding Droplets", Description="Welcome to coding droplets"},
-            new CollectionItem{Imagen ="dell.png",  Title="Title 2", Description="Description 2"},
-            new CollectionItem{Imagen = "dell.png", Title="Title 3", Description="Description 3"},
-            new CollectionItem{Imagen ="acer_i5.jpg", Title="Title 4", Description="Description 4"},
-            new CollectionItem{Imagen = "ace_rycen5.jpg", Title="Title 5", Description="Description 5"},
-            new CollectionItem{Imagen = "acer_i5.jpg", Title="Title 5", Description="Description 5"}
+            new CollectionItem{Imagen ="ace_rycen5.jpg", Title="Acer Gaming", Description="Tenemos lo mejor en computadoras"},
+            new CollectionItem{Imagen ="dell.png",  Title="Del Rycen-i7", Description="Tenemos lo mejor en computadoras"},
+            new CollectionItem{Imagen = "dell.png", Title="Dell Rycen-i3", Description="Tenemos lo mejor en computadoras"},
+            new CollectionItem{Imagen ="acer_i5.jpg", Title="Acer Gaming-12Gen", Description="Tenemos lo mejor en computadoras"},
+            new CollectionItem{Imagen = "ace_rycen5.jpg", Title="Acer Rycen-5", Description="Tenemos lo mejor en computadoras"},
+            new CollectionItem{Imagen = "acer_i5.jpg", Title="Lenovo", Description="Tenemos lo mejor en computadoras"}
         };
         carouselView.ItemsSource = items;
 
